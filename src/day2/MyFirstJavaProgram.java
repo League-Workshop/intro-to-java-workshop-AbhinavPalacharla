1a49be18090126d1bd4990f0c00f55beb101d650
themasterofiron <owendouglas0528>
2017-11-20 13:51:46 -0800

    workshop

--- src/day2/MyFirstJavaProgram.java
+++ src/day2/MyFirstJavaProgram.java
@@ -8,7 +8,30 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+	Robot r2d2 = new Robot(); 
+		
 	
+		r2d2.setSpeed(90);
+		r2d2.setPenColor(Color.red);
+		r2d2.penDown();
+		r2d2.move(100);
+		r2d2.turn(90);
+		r2d2.penUp();
+		r2d2.setPenColor(Color.blue);
+		r2d2.penDown();
+		r2d2.move(100);
+		r2d2.turn(90);
+		r2d2.penUp();
+		r2d2.setPenColor(Color.red);
+		r2d2.penDown();
+		r2d2.move(100);
+		r2d2.turn(90);
+		r2d2.penUp();
+		r2d2.setPenColor(Color.blue);
+		r2d2.penDown();
+		r2d2.move(100);
+		r2d2.turn(90);
+		r2d2.penUp();
 		
 		
 	}
--- src/day2/Spiral.java
+++ src/day2/Spiral.java
@@ -7,19 +7,23 @@ public class Spiral {
 	public static void main(String[] args) {
 		// 1. Create a new Robot
 		
+		Robot walle = new Robot();
+		
 		// 5. Set your robot's pen to the down position
-		
+		walle.penDown();
 		// 3. Set the robot to go at max speed (10)
-		
+		walle.setSpeed(100);
 		// 4. Do the following (steps 6-8) 75 times
+		for (int j = 0; j < 1000000; j++) {
+			
 		
 			// 7. Change the pen color to random
-		
+		walle.setRandomPenColor();
 			// 6. Move the robot 5 times the current line number you are drawing (5*i)
-			
+			walle.move(5*j);
 			// 2. Turn the robot 1/7 of 360 degrees to the right
-		
+		walle.turn(360/7);
 			// 8. Set the pen width to i
-			
+			walle.setPenWidth(j);}
 	}
 }
